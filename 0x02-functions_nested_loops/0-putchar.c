ed15dcc0baccb987bec9ee3754aff3c260f64100
Shatega10 <shatega10@gmail.com>
2022-06-08 21:41:56 -0700

    My ini commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
-#include "main.h"
-/** 
-* main - Entry point
-* Return:0 
-*/ 
-int main(void) 
-{ 
-char c[10] = "_putchar"; 
-int i = 0; 
-while (i < 9) 
-{ 
-_putchar(c[i]); 
-i++;
-} 
-_putchar('\n'); 
-return (0); 
+#include <main.h>
+/**
+* main - program that prints _putchar.
+*
+* Return: int
+*/
+int main(void)
+{
+int n = 0;
+_putchar(char c);
+n += 1;
+while (n < 9)
+{
+_putchar('\n');
+return (0);
 }
